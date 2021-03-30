61e330e1cbe6bb73b0b05d3b6f2e925954e50f16
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2021-03-30 18:52:25 +0300

    add Sight model and some mock data

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  String name;
+  double lat;
+  double lot;
+  String url;
+  String details;
+  String type;
+
+  Sight({this.name, this.lat, this.lot, this.url, this.details, this.type});
+}
